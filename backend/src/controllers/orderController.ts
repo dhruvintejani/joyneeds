f6da49c07190af813221409befcb8e3967db09a6
dhruvintejani <156985917+dhruvintejani@users.noreply.github.com>
2026-09-07 09:11:13 +0530

    Phase 6: add order controllers

---
+++ backend/src/controllers/orderController.ts
@@ -0,0 +1,18 @@
+import type { RequestHandler } from "express";
+import { createPendingOrder, getCustomerOrder, listCustomerOrders } from "../services/orderService.js";
+import type { CreateOrderBody, CustomerOrderParams } from "../validators/orderValidators.js";
+
+export const createOrderController: RequestHandler = async (_req, res) => {
+  const clerkUserId = (res.locals.clerkUserId as string | undefined) ?? null;
+  const order = await createPendingOrder(res.locals.validatedBody as CreateOrderBody, clerkUserId);
+  res.status(201).json({ data: order });
+};
+
+export const listCustomerOrdersController: RequestHandler = async (_req, res) => {
+  res.json({ data: await listCustomerOrders(res.locals.clerkUserId as string) });
+};
+
+export const getCustomerOrderController: RequestHandler = async (_req, res) => {
+  const { orderNumber } = res.locals.validatedParams as CustomerOrderParams;
+  res.json({ data: await getCustomerOrder(res.locals.clerkUserId as string, orderNumber) });
+};
